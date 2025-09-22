6b38df6d209ab0ab38f2fb15ed2bc949c7c3815c
pikamar <mareks.pikalovs@gmail.com>
2025-09-22 11:41:46 +0300

    Update dalle-client.py

--- Labfiles/dalle-client/python/dalle-client.py
+++ Labfiles/dalle-client/python/dalle-client.py
@@ -2,6 +2,10 @@ import os
 import json
 
 # Add references
+from dotenv import load_dotenvfrom azure.identity 
+import DefaultAzureCredential, get_bearer_token_provider
+from openai import AzureOpenAI
+import requests
 
 
 def main(): 
@@ -18,9 +22,17 @@ def main():
         api_version = os.getenv("API_VERSION")
         
         # Initialize the client
-        
-        
+        token_provider = get_bearer_token_provider(
+            DefaultAzureCredential(exclude_environment_credential=True,
+                exclude_managed_identity_credential=True), 
+            "https://cognitiveservices.azure.com/.default"
+        )
 
+        client = AzureOpenAI(
+            api_version=api_version,
+            azure_endpoint=endpoint,
+            azure_ad_token_provider=token_provider
+        )
          
         img_no = 0
         # Loop until the user types 'quit'
@@ -34,6 +46,14 @@ def main():
                 continue
             
             # Generate an image
+            result = client.images.generate(
+                model=model_deployment,
+                prompt=input_text,
+                n=1
+            )
+
+            json_response = json.loads(result.model_dump_json())
+            image_url = json_response["data"][0]["url"] 
             
 
             # save the image
